cc4fd0091f2e60425afa1cfba5f67c68a29b9558
null-- <sinahatef.cpp@gmail.com>
2014-08-11 14:14:27 +0430

    top/btm

--- status.sh
+++ status.sh
@@ -6,4 +6,7 @@ gap=16
 # status_width= 820
 # conky -c ~/i3/dzconky.cfg | dzen2 -ta l -sa c -w $status_width -x $status_x -y $status_y -fg white -bg '#050505' -m -fn '-*-lucidatypewriter-medium-*-*-*-12-*-*-*-*-*-*-*' || exit 1
 
+# - bottom
+# conky -c ~/i3/dzconky.cfg | dzen2 -ta l -sa c -w 820 -x 250 -y 1024 -fg white -bg '#000000' -m -fn '-*-lucidatypewriter-medium-*-*-*-12-*-*-*-*-*-*-*' || exit 1
+# - top
 conky -c ~/i3/dzconky.cfg | dzen2 -ta l -sa c -w 820 -x 250 -y 0 -fg white -bg '#000000' -m -fn '-*-lucidatypewriter-medium-*-*-*-12-*-*-*-*-*-*-*' || exit 1
