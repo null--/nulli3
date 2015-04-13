8e9584c28dbfaaa41a4a78e9afffaf0b65dda31b
null-- <sinahatef.cpp@gmail.com>
2015-04-13 11:35:00 +0430

    imprv

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ---- First time init ---- #
+if ! [ -e ~/.i3/initialized ] 
+then
+    echo "DELETE THIS FILE TO RE-INITIALIZE nulli3" > ~/.i3/initialized
+    
+    # ---- Init ----
+    # -- Set Wallpaper
+    nitrogen --set-auto ~/.i3/wallpaper/anarchy-g.jpg
+fi
+
