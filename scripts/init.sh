75a415555645c27a40504cdc018513eb6e7c355f
_null_ <_null_@example.com>
2016-05-08 12:10:41 +0430

    reorg

---
+++ scripts/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ---- First time init ---- #
+if ! [ -e ~/.i3/scripts/initialized ] 
+then
+    echo "DELETE THIS FILE TO RE-INITIALIZE nulli3" > ~/.i3/scripts/initialized
+
+    # ---- Init ----
+    # -- Set Wallpaper
+    nitrogen --save --set-auto ~/.i3/wallpapers/anarchy-g.jpg
+fi
+
