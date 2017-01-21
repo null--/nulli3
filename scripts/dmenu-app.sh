cc242558c03f5f4347bafe43d08b88b6ec997602
null-- <sinahatef.cpp@gmail.com>
2017-01-21 11:41:32 +0330

    improvement

--- scripts/dmenu-app.sh
+++ scripts/dmenu-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # i3-dmenu-desktop --dmenu='dmenu -i -b -fn "Monospace-8" -p "App: "'
-i3-dmenu-desktop --dmenu='dmenu -i -b -fn "Roboto-12" -p "App: "'
+i3-dmenu-desktop --dmenu='dmenu -i -b -fn "Roboto-16" -p "App: "'
